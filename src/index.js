a02bef391f6d3f4330e728c411e3349b4d222ddf
karma <karmadrukpa11@gmail.com>
2019-05-02 19:48:51 -0700

    added on and emit  functions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 const path = require('path')
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io')
 
 const app = express()
+// created server
+const server = http.createServer(app)
+// creating instance of socket.io
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+let count = 0;
+
+io.on('connection', (socket) => {
+    console.log('new websocket connection')
+
+    // emit accept eventname as a frist argumment
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++
+        io.emit('countUpdated', count)
+    })
+
+})
+
+// starting up server
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
